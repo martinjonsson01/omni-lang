4744d47b886df88113344c4488b2370b4e65cddc
Martin <martinjonsson01@gmail.com>
2024-04-27 18:03:54 +0200

    Parse compiler options

--- omni-cli/app/Main.hs
+++ omni-cli/app/Main.hs
@@ -1,32 +1,83 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Main (main) where
 
 import Import
-import Run
-import RIO.Process
 import Options.Applicative.Simple
 import qualified Paths_omni_cli
+import RIO.Process
+import Run
 
 main :: IO ()
 main = do
-  (options, ()) <- simpleOptions
-    $(simpleVersion Paths_omni_cli.version)
-    "Header for command line arguments"
-    "Program description, also for command line arguments"
-    (Options
-       <$> switch ( long "verbose"
-                 <> short 'v'
-                 <> help "Verbose output?"
-                  )
-    )
-    empty
+  (options, ()) <-
+    simpleOptions
+      $(simpleVersion Paths_omni_cli.version)
+      "Omni Compiler"
+      "A compiler for the Omni programming language."
+      ( Options
+          <$> inputParser
+          <*> outputParser
+          <*> verbosityParser
+          <*> optLevelParser
+      )
+      empty
   lo <- logOptionsHandle stderr (optionsVerbose options)
   pc <- mkDefaultProcessContext
   withLogFunc lo $ \lf ->
-    let app = App
-          { appLogFunc = lf
-          , appProcessContext = pc
-          , appOptions = options
-          }
+    let app =
+          App
+            { appLogFunc = lf
+            , appProcessContext = pc
+            , appOptions = options
+            }
      in runRIO app run
+
+inputParser :: Parser Input
+inputParser =
+  argument (InputFile <$> str)
+    $ metavar "SOURCE_FILE"
+    <> help
+      "Source file to compile. \
+      \ If not given, input is read from standard input."
+    <> value StdIn
+
+outputParser :: Parser Output
+outputParser =
+  option (OutputFile <$> str)
+    $ short 'o'
+    <> long "output-file"
+    <> metavar "FILE"
+    <> help
+      "Output file for the compilation result. \
+      \ If not given, output is written to standard output."
+    <> value StdOut
+
+verbosityParser :: Parser Bool
+verbosityParser =
+  switch
+    ( long "verbose"
+        <> short 'v'
+        <> help "Verbose output?"
+    )
+
+optLevelParser :: Parser OptimizationLevel
+optLevelParser =
+  option parseOptLevel
+    $ short 'O'
+    <> metavar "LEVEL"
+    <> help
+      "Optimization level. \
+      \ If not given, no optimization is applied (i.e. -O0)."
+    <> value O0
+ where
+  parseOptLevel :: ReadM OptimizationLevel
+  parseOptLevel = do
+    level <- auto
+    case level :: Int of
+      0 -> return O0
+      1 -> return O1
+      2 -> return O2
+      3 -> return O3
+      _ -> readerError "Valid optimization levels are 0 - 3"
--- omni-cli/src/Types.hs
+++ omni-cli/src/Types.hs
@@ -1,25 +1,41 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-module Types
-  ( App (..)
-  , Options (..)
-  ) where
+
+module Types (
+  App (..),
+  Options (..),
+  Input (..),
+  Output (..),
+  OptimizationLevel(..),
+) where
 
 import RIO
 import RIO.Process
 
--- | Command line arguments
+-- | Command line arguments.
 data Options = Options
-  { optionsVerbose :: !Bool
+  { optionsInput :: !Input
+  , optionsOutput :: !Output
+  , optionsVerbose :: !Bool
+  , optionsOpt :: !OptimizationLevel
   }
 
+-- | Compiler input modes.
+data Input = StdIn | InputFile !FilePath
+
+-- | Compiler output modes.
+data Output = StdOut | OutputFile !FilePath
+
+-- | Which level of optimization to apply.
+data OptimizationLevel = O0 | O1 | O2 | O3
+
+-- | Application config.
 data App = App
   { appLogFunc :: !LogFunc
   , appProcessContext :: !ProcessContext
   , appOptions :: !Options
-  -- Add other app-specific configuration information here
   }
 
 instance HasLogFunc App where
-  logFuncL = lens appLogFunc (\x y -> x { appLogFunc = y })
+  logFuncL = lens appLogFunc (\x y -> x{appLogFunc = y})
 instance HasProcessContext App where
-  processContextL = lens appProcessContext (\x y -> x { appProcessContext = y })
+  processContextL = lens appProcessContext (\x y -> x{appProcessContext = y})
